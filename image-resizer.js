6cc5f753c1f4c16fd9ef4ac68cf43c820916e2c5
Andrés Proaño <andres@oventi.org>
2014-02-16 21:38:31 -0300

    separated functionality in two objects, updated example and readme

--- image-resizer.js
+++ image-resizer.js
@@ -1,87 +1,132 @@
-function ImageResizer(file, factor) {
-    this.resize_save_path = "../image-resizer.php";
-    this.save_folder = "result";
-    
-    var self = this;
-    self.file = file;
-    var steps = [0.5, 0.25, 0.125];
-    resize_factor = 0;
-    
-    if(factor < 1) {
-        resize_factor = factor;
-    }
-    
-    var resize_image = function (loop, count, factor, when_done) {
-        var w = self.image.width;
-        var h = self.image.height;
+/*
+This file is part of image-resizer.
 
-        w = w * factor; h = h * factor;
+image-resizer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-        var canvas = document.createElement("canvas");
-        canvas.setAttribute("width", w);
-        canvas.setAttribute("height", h);
+image-resizer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-        var context = canvas.getContext("2d");
-        context.drawImage(self.image, 0, 0, w, h);
+You should have received a copy of the GNU General Public License
+along with image-resizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-        var new_image = new Image();
-        new_image.onload = function () {
-            self.image = new_image;
+var oventi = oventi || {};
 
-            if(loop < count) {
-                resize_image(loop+1, count, factor, when_done);
-            }
-            else {
-                when_done();
-            }
-        }
-        new_image.src = canvas.toDataURL("image/jpeg", 1.0);
-    }
-
-    var start_resizing = function (final_step) {
-        var loop_count = 0;
-        for(var p in steps) {
-            if(steps[p] < resize_factor) break;
-            loop_count++;
+oventi.ImageResizer = {
+    Resized: function(data) {
+        this.save_path = ".";
+        this.post_url = "./image-resizer.php";
+        
+        this.get_url = function () {
+            return data.canvas.toDataURL(data.file.type, 1.0);
         }
         
-        var remaining = resize_factor / steps[loop_count-1];
-        
-        resize_image(1, loop_count, 0.5, function () {
-            if(remaining < 1) {
-                resize_image(1, 1, remaining, function () { final_step(); });
-            }
-            else { final_step(); }
-        });
-    }
-    
-    this.resize = function (resize_done) {
-        var fr = new FileReader();
-        fr.onload = function (e) {
-            self.data_url = e.target.result;
+        this.save = function (save_done) {
+            var post_data = {
+                file_name: data.file.name,
+                data: this.get_url(),
+                save_path: this.save_path
+            };
             
-            var image = new Image();
-            image.onload = function () {
-                self.image = this;
-                if(factor > 1) {
-                    resize_factor = factor / self.image.width;
-                }
+            $.post(this.post_url, post_data, function (response) {
+                save_done(response);
+            }, 'json');
+        }
+    },
+    
+    Resizer: function () {
+        this.resize_save_path = "../image-resizer.php";
+        this.save_folder = "result";
+        this.resize_factor = 0;
+        this.canvas = null;
+
+        var self = this;
+        var steps = [0.5, 0.25, 0.125];
+        
+        var resize_image = function (loop, count, factor, when_done) {
+            var w = self.image.width;
+            var h = self.image.height;
+
+            w = w * factor; h = h * factor;
+
+            var canvas = document.createElement("canvas");
+            canvas.setAttribute("width", w);
+            canvas.setAttribute("height", h);
+            self.canvas = canvas;
+            
+            var context = canvas.getContext("2d");
+            context.drawImage(self.image, 0, 0, w, h);
+            
+            if(count == 1) {
+                return null;
+            }
+            
+            var new_image = new Image();
+            new_image.onload = function () {
+                self.image = new_image;
                 
-                start_resizing(function () {
-                    resize_done(self.image.src);
+                if(loop < count) {
+                    resize_image(loop+1, count, factor, when_done);
+                }
+                else {        
+                    when_done();
+                }
+            }
+            new_image.src = canvas.toDataURL("image/jpeg", 1.0);
+        }
+
+        var start_resizing = function (final_step) {
+            var loop_count = 0;
+            for(var p in steps) {
+                if(steps[p] < self.resize_factor) break;
+                loop_count++;
+            }
+
+            var remaining = self.resize_factor / steps[loop_count-1];
+            
+            if(loop_count > 1) {
+                resize_image(1, loop_count, 0.5, function () {
+                    if(remaining < 1) {
+                        resize_image(1, 1, remaining, null);
+                        final_step();
+                    }
+                    else { final_step(); }
                 });
             }
-            image.src = self.data_url;
+            else {
+                resize_image(1, 1, 0.5, null);
+                final_step();
+            }            
         }
-        fr.readAsDataURL(self.file);
+        
+        this.resize = function (file, resize_factor, resize_done) {
+            self.resize_factor = resize_factor;
+            
+            var fr = new FileReader();
+            fr.onload = function (e) {
+                self.data_url = e.target.result;
+
+                var image = new Image();
+                image.onload = function () {
+                    self.image = this;
+                    if(self.resize_factor > 1) {
+                        self.resize_factor = self.resize_factor / self.image.width;
+                    }
+                    
+                    start_resizing(function (canvas) {
+                        var data = { file: file, canvas: self.canvas };
+                        var resized = new oventi.ImageResizer.Resized(data);
+                        resize_done(resized);
+                    });
+                }
+                image.src = self.data_url;
+            }
+            fr.readAsDataURL(file);
+        }
     }
-    
-    this.resize_save = function (resize_done) {
-        this.resize(function () {
-            var data = {ir_name: self.file.name, ir_data: self.image.src, ir_folder: self.save_folder};
-            $.post(self.resize_save_path, data, function (response) {
-                resize_done(response);
-            }, 'json');
-        });
-    }
-}
+};
--- image-resizer.php
+++ image-resizer.php
@@ -1,19 +1,37 @@
 <?php
 
-class ImageResizer {
-    public function save($folder) {
-        $file_name = $_POST['ir_name'];
-        $parts = explode(';base64,', $_POST['ir_data']);
-        $image_data = $parts[1];
-        
-        file_put_contents($folder . '/' . $file_name, base64_decode($image_data));
-        
-        echo json_encode(true);
-        exit;
-    }
+/*
+This file is part of image-resizer.
+
+image-resizer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+image-resizer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with image-resizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+error_reporting(0);
+
+$file_name = $_POST['file_name'];
+$save_path = $_POST['save_path'];
+$parts = explode(';base64,', $_POST['data']);
+$image_data = $parts[1];
+$response = array();
+
+if(file_put_contents($save_path . '/' . $file_name, base64_decode($image_data))) {
+    $response['ok'] = true;
+    $response['file_name'] = $file_name;
+}
+else {
+    $response['ok'] = false;
 }
 
-if(isset($_POST['ir_name']) && isset($_POST['ir_data']) && isset($_POST['ir_folder'])) {
-    $ir = new ImageResizer();
-    $ir->save($_POST['ir_folder']);
-}
+echo json_encode($response);
+exit;
